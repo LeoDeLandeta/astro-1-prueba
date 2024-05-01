190b32c239cd747727bce2cb056ce4a85a2f835e
leodelandeta <116042126+LeoDeLandeta@users.noreply.github.com>
2024-05-01 19:24:51 -0300

    Add files via upload

---
+++ src/data/books.js
@@ -0,0 +1,31 @@
+export const books = [
+    {
+        id:'clean-code',
+        title:'Clean Code : Manual de dearrollo ágil de software',
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg",
+        opinion:'Un clásico entre los libros del mundo de programación',
+        author:'Robert C. Martin',
+    },
+    {
+        id:'the-clean-code',
+        title:'the Clean Code :A code of conduct for Professional Programmers',
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg",
+        opinion:'',
+        author:'Robert C. Martin',
+    },
+    {
+        id:'javascript-the-good-parts',
+        title:'javascript:the good parts',
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1328834793i/2998152.jpg",
+        opinion:'',
+        author:'Douglas Crockford',
+    },
+    {
+        id:'eloquent-javascript',
+        title:'Eloquent JavaScript: A Morderm Introduction to programming',
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1308260856i/8910666.jpg",
+        opinion:'',
+        author:'Marijin Haverbeke',
+    }
+
+]
